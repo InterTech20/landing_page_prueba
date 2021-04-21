1ea5cb64a46ff0a1976016ffe1f989a8112e47be
Jhonathan O'Brian Saa Se <64106872+InterTech20@users.noreply.github.com>
2021-04-21 18:57:13 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+document.getElementById('container_nav').addEventListener('click',function () {
+    document.querySelector('#nav').classList.toggle('menu_conten');
+});
+document.getElementById('item1').addEventListener('mouseover',function () {
+    document.getElementById("img").src="1.jpg";
+});
+document.getElementById('item2').addEventListener('mouseover',function () {
+    document.getElementById("img").src="img.png";
+});
+document.getElementById('item3').addEventListener('mouseover',function () {
+    document.getElementById("img").src="0.jpg";
+});
