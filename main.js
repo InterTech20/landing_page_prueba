204f1736d0de80ed8ee2ee11d9be45e271d2c3b5
Jhonathan O'Brian Saa Se <64106872+InterTech20@users.noreply.github.com>
2021-05-04 03:15:57 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
+let cont=0;
+
 document.getElementById('container_nav').addEventListener('click',function () {
     document.querySelector('#nav').classList.toggle('menu_conten');
+    if(cont==0){  
+        cont=1;
+        document.querySelector('#icon_menu').classList.replace('fa-bars','fa-times');
+        document.querySelector('.menu').style.background='#fbfbfb';
+    }else{
+        cont=0; 
+        document.querySelector('#icon_menu').classList.replace('fa-times','fa-bars');
+        document.querySelector('.menu').style.background='transparent';
+       
+    }
+    
+  
 });
 document.getElementById('item1').addEventListener('mouseover',function () {
-    document.getElementById("img").src="1.jpg";
+    document.getElementById("img").src="0.jpg";
 });
 document.getElementById('item2').addEventListener('mouseover',function () {
-    document.getElementById("img").src="img.png";
+    document.getElementById("img").src="1.jpg";
 });
 document.getElementById('item3').addEventListener('mouseover',function () {
-    document.getElementById("img").src="0.jpg";
+    document.getElementById("img").src="img.png";
 });
